6b8bc33a90fa5ba1f86867c9e070af56a2866e26
Radekkstach <97514874+Radekkstach@users.noreply.github.com>
2023-06-07 08:50:31 +0200

    Add files via upload

---
+++ testVyjimkyStach/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testVyjimkyStach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double prumer = 0;
+            int pocitadlo = 0;
+            string[] text = textBox1.Lines;
+            foreach(string radek in text)
+            {
+                try
+                {
+                    int cislo = Convert.ToInt32(radek);
+                    if (cislo < 0)
+                    {
+                        prumer = prumer + cislo;
+                        pocitadlo++;
+                    }
+                }
+                catch(FormatException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                catch(OverflowException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                
+            }
+            try
+            {
+                prumer = prumer / pocitadlo;
+            }
+            catch(OverflowException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch(DivideByZeroException)
+            {
+                
+            }
+            label1.Text = "Prumer je: " + prumer; 
+        }
+    }
+}
